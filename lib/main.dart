370fc6765c7f6ef9ea740525387f0624cac6e11d
Shinta Yulistiana <shintayulistiana2004@gmail.com>
2024-09-29 21:18:53 +0700

    add image_03

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widgets/image_widget.dart';
 import 'package:hello_world/basic_widgets/text_widget.dart';
 
 void main() {
@@ -107,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const MyTextWidget(),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
